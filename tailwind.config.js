ccd42f6f0c914c0f93fab7c837a976ced74e1e7d
AbuBokorprog <abubokor1066@gmail.com>
2024-02-06 21:31:07 +0600

    assets import and color setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,15 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: {
+      blushPink: "#FFB6C1",
+      secondary: "#E6E6FA",
+      accent: `#FF6F61`,
+      BoldRed: `#FF0000`,
+      Teal: `#008080`,
+      Turquoise: `#40E0D0`,
+      main: "#D3D3D3",
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
