ef8b9957c6c1b2c08b5ff16c01a73ef803dd1a4f
AbuBokorprog <abubokor1066@gmail.com>
2024-02-12 14:22:11 +0600

    Products model created

---
+++ models/products.js
@@ -0,0 +1,55 @@
+const mongoose = require("mongoose");
+
+const productsSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  userId: {
+    type: String,
+    required: true,
+  },
+  image1: {
+    type: String,
+    required: true,
+  },
+  image2: {
+    type: String,
+  },
+  image3: {
+    type: String,
+  },
+  image4: {
+    type: String,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  sub_category: {
+    type: String,
+    required: true,
+  },
+  concern: {
+    type: String,
+  },
+  offer: {
+    type: String,
+  },
+  brands: {
+    type: String,
+    required: true,
+  },
+  quantity: {
+    type: Number,
+    required: true,
+  },
+  status: {
+    type: String,
+    enum: ["pending", "approved", "declined"],
+  },
+});
+
+const Products =
+  mongoose.models.products || mongoose.model("Products", productsSchema);
+export default Products;
