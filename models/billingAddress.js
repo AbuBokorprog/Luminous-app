707e4cc44ee687bc69f3b66af5afa77b46d50ec5
AbuBokorprog <abubokor1066@gmail.com>
2024-02-12 22:04:12 +0600

    billingAddress and shippingAddress model created

---
+++ models/billingAddress.js
@@ -0,0 +1,51 @@
+const mongoose = require("mongoose");
+
+const billingAddressSchema = mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  userId: {
+    type: String,
+    required: true,
+  },
+  companyName: {
+    type: String,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  country: {
+    type: String,
+    required: true,
+  },
+  street: {
+    type: String,
+    required: true,
+  },
+  Phone: {
+    type: Number,
+    required: true,
+  },
+  postcode: {
+    type: Number,
+  },
+  town: {
+    type: String,
+    required: true,
+  },
+  state: {
+    type: Number,
+    required: true,
+  },
+});
+
+const BillingAddress =
+  mongoose.models.BillingAddress ||
+  mongoose.model("BillingAddress", billingAddressSchema);
+export default BillingAddress;
