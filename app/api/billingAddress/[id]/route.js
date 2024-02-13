34f709ee9a3af41959df0656a530b6d06c98455e
AbuBokorprog <abubokor1066@gmail.com>
2024-02-13 10:00:06 +0600

    billingAddress and shippingAddress get, put and delete implement successfully

---
+++ app/api/billingAddress/[id]/route.js
@@ -0,0 +1,92 @@
+import BillingAddress from "@/models/billingAddress";
+import { database } from "@/utils/database/database";
+import { NextResponse } from "next/server";
+
+database();
+export async function GET(_, { params }) {
+  const { id } = params;
+  try {
+    const billingAddress = await BillingAddress.findById({ _id: id });
+    return NextResponse.json(billingAddress);
+  } catch (error) {
+    return NextResponse.json({
+      error: `Specific billing address get failed: ${error.message}`,
+      status: false,
+    });
+  }
+}
+export async function PUT(req, { params }) {
+  const { id } = params;
+  const {
+    firstName,
+    lastName,
+    userId,
+    companyName,
+    country,
+    street,
+    state,
+    town,
+    postCode,
+    phone,
+    email,
+  } = await req.json();
+  try {
+    const updateBillingAddress = await BillingAddress.findOneAndUpdate(
+      { _id: id },
+      {
+        firstName,
+        lastName,
+        userId,
+        companyName,
+        country,
+        street,
+        state,
+        town,
+        postCode,
+        phone,
+        email,
+      }
+    );
+    if (!updateBillingAddress) {
+      return NextResponse.json({
+        error: "Could not find billing address",
+        status: false,
+      });
+    }
+    return NextResponse.json({
+      message: "billing address updated successfully",
+      status: true,
+    });
+  } catch (error) {
+    return NextResponse.json({
+      error: `Could not update billing address ${error.message}`,
+      status: false,
+    });
+  }
+}
+
+export async function DELETE(_, { params }) {
+  const { id } = params;
+  try {
+    const deleteBillingAddress = await BillingAddress.findOneAndDelete({
+      _id: id,
+    });
+    if (!deleteBillingAddress) {
+      return NextResponse.json({
+        error: `Could not find Billing address`,
+        status: false,
+      });
+    }
+
+    return NextResponse.json({
+      message: `Billing address deleted successfully`,
+      status: true,
+      deleteBillingAddress,
+    });
+  } catch (error) {
+    return NextResponse.json({
+      error: `Billing address deleted failed: ${error.message}`,
+      status: false,
+    });
+  }
+}
---
+++ app/api/shippingAddress/[id]/route.js
@@ -0,0 +1,92 @@
+import ShippingAddress from "@/models/shippingAddress";
+import { database } from "@/utils/database/database";
+import { NextResponse } from "next/server";
+
+database();
+export async function GET(_, { params }) {
+  const { id } = params;
+  try {
+    const shippingAddress = await ShippingAddress.findById({ _id: id });
+    return NextResponse.json(shippingAddress);
+  } catch (error) {
+    return NextResponse.json({
+      error: `Specific shipping address get failed: ${error.message}`,
+      status: false,
+    });
+  }
+}
+export async function PUT(req, { params }) {
+  const { id } = params;
+  const {
+    firstName,
+    lastName,
+    userId,
+    companyName,
+    country,
+    street,
+    state,
+    town,
+    postCode,
+    phone,
+    email,
+  } = await req.json();
+  try {
+    const updateShippingAddress = await ShippingAddress.findOneAndUpdate(
+      { _id: id },
+      {
+        firstName,
+        lastName,
+        userId,
+        companyName,
+        country,
+        street,
+        state,
+        town,
+        postCode,
+        phone,
+        email,
+      }
+    );
+    if (!updateShippingAddress) {
+      return NextResponse.json({
+        error: "Could not find shipping address",
+        status: false,
+      });
+    }
+    return NextResponse.json({
+      message: "Shipping address updated successfully",
+      status: true,
+    });
+  } catch (error) {
+    return NextResponse.json({
+      error: `Could not update shipping address ${error.message}`,
+      status: false,
+    });
+  }
+}
+
+export async function DELETE(_, { params }) {
+  const { id } = params;
+  try {
+    const deleteShippingAddress = await ShippingAddress.findOneAndDelete({
+      _id: id,
+    });
+    if (!deleteShippingAddress) {
+      return NextResponse.json({
+        error: `Could not find shipping address`,
+        status: false,
+      });
+    }
+
+    return NextResponse.json({
+      message: `Shipping address deleted successfully`,
+      status: true,
+      deleteShippingAddress,
+    });
+  } catch (error) {
+    return NextResponse.json({
+      error: `Shipping address deleted failed: ${error.message}`,
+      status: false,
+    });
+  }
+}
