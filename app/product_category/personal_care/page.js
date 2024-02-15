3e6147ebc99e1e7035ad90536be05f8b6e0c99dc
AbuBokorprog <abubokor1066@gmail.com>
2024-02-15 13:56:15 +0600

    personal care page created

---
+++ app/product_category/personal_care/page.js
@@ -0,0 +1,10 @@
+import PersonalCare from "@/components/productCategory/personalCare";
+import React from "react";
+
+export default function page() {
+  return (
+    <div>
+      <PersonalCare />
+    </div>
+  );
+}
